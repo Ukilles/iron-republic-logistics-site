1bb4238c3e0e58eb633a2bc37443caa83e271318
Ukilles <ukilles@gmail.com>
2026-03-26 23:11:11 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="site-body">
+        {children}
+      </body>
     </html>
   );
 }
